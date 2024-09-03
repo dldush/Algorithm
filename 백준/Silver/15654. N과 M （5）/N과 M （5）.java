f0e76650d4163e9b704023437dc061d5e4f03b1a
dldush <leecarus0301@naver.com>
2024-09-03 17:06:23 +0900

    [Silver III] Title: N과 M (5), Time: 2072 ms, Memory: 72320 KB -BaekjoonHub

---
+++ 백준/Silver/15654. N과 M （5）/N과 M （5）.java
@@ -0,0 +1,50 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+	static int N,M;
+	static int []arr,numbers;
+	static boolean[] visited;
+
+	public static void main(String[] args) {
+		Scanner sc =new Scanner(System.in);
+		
+		N=sc.nextInt();
+		M=sc.nextInt();
+		
+		arr=new int[M];
+		numbers=new int[N];
+		visited=new boolean[N];
+		
+		for(int i=0; i<N; i++) {
+			numbers[i]=sc.nextInt();
+		}
+		Arrays.sort(numbers);
+		
+		perm(0);
+
+	}
+
+	static void perm(int cnt) {
+		if(cnt==M) {
+			for(int i=0; i<M; i++) {
+				System.out.print(arr[i]+" ");
+			}System.out.println();
+			return;
+		}
+		
+		for(int i=0; i<N; i++) {
+			if(!visited[i]) {
+				visited[i]=true;
+				arr[cnt]=numbers[i];
+				perm(cnt+1);
+				visited[i]=false;
+			}
+		}
+		
+	}
+
+	
+
+
+}
