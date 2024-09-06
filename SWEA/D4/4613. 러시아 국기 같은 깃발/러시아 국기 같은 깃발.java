ef0d604db38038f180f7c730241712cdf5a0fb44
dldush <leecarus0301@naver.com>
2024-09-06 14:45:00 +0900

    [D4] Title: 러시아 국기 같은 깃발, Time: 238 ms, Memory: 22,260 KB -BaekjoonHub

--- SWEA/D4/4613. 러시아 국기 같은 깃발/러시아 국기 같은 깃발.java
+++ SWEA/D4/4613. 러시아 국기 같은 깃발/러시아 국기 같은 깃발.java
@@ -1,63 +1,63 @@
+
 import java.util.Scanner;
 
 public class Solution {
 
 	public static void main(String[] args) {
-
 		Scanner sc = new Scanner(System.in);
-
-		int T = sc.nextInt();
-
-		for (int tc = 1; tc <= T; tc++) {
-
-			int N = sc.nextInt();
-			int M = sc.nextInt();
-
-			String[][] flag = new String[N][M];
-			
-			for(int n = 0 ; n < N ; n++) {
-				String str = sc.next();
-				for(int m = 0 ; m < M ; m++) {
-					flag[n][m] = String.valueOf(str.charAt(m));
-				}
-			}
-			
-
-			int[][] count = new int[N][3];
-
-			for (int n = 0; n < N; n++) {
-				for (int m = 0; m < M; m++) {
-					if (flag[n][m].equals("W")) {
-						count[n][0]++;
-					} else if (flag[n][m].equals("B")) {
-						count[n][1]++;
-					} else if (flag[n][m].equals("R")) {
-						count[n][2]++;
-					}
-				}
-			}
-			int answer = Integer.MAX_VALUE;
-
-			for (int wRow = 0; wRow < N - 2; wRow++) {
-				for (int bRow = wRow + 1; bRow < N - 1; bRow++) {
-					int rRow = N - 1;
-					int changes = 0;
-
-					for (int i = 0; i <= wRow; i++) {
-						changes += M - count[i][0];
-					}
-					for (int i = wRow + 1; i <= bRow; i++) {
-						changes += M - count[i][1];
-					}
-					for (int i = bRow+1; i <= rRow ; i++) {
-						changes += M - count[i][2];
-					}
-					answer = Math.min(changes, answer);
-				}
-			}
-			System.out.println("#"+tc+" "+answer);
-		} // tc end
 		
+		int T = sc.nextInt();
+		
+		for(int tc=1; tc<=T; tc++) {
+			int N=sc.nextInt();
+			int M=sc.nextInt();
+			
+			char[][]arr = new char [N][M];
+			
+			for(int n=0; n<N; n++) {
+				String str = sc.next();
+				for(int m=0; m<M; m++) {
+					arr[n][m]=str.charAt(m);
+				}
+			}
+			
+			int[][]color = new int[N][3];
+			for(int n=0; n<N; n++) {
+				for(int m=0; m<M; m++) {
+					if(arr[n][m]=='W') {
+						color[n][0]++;
+					}else if(arr[n][m]=='B') {
+						color[n][1]++;
+					}else if(arr[n][m]=='R') {
+						color[n][2]++;
+					}
+				}
+			}
+			
+			int min = Integer.MAX_VALUE;
+			
+			for(int bRow=0; bRow<N-2; bRow++) {
+				for(int rRow=bRow+1; rRow<N-1; rRow++) {
+					int wRow = N-1;
+					int changes = 0;
+					
+					for(int i=0; i<=bRow; i++) {
+						changes += M - color[i][0];
+					}
+					
+					for(int i=bRow+1; i<=rRow; i++) {
+						changes += M - color[i][1];
+					}
+					
+					for(int i=rRow+1; i<=wRow; i++) {
+						changes += M - color[i][2];
+					}
+					
+					min = Math.min(changes, min);
+				}
+			}
+			
+			System.out.println("#"+tc+" "+min);
+		}
 	}
-
 }
