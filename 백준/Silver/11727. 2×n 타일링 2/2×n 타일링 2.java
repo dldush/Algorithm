809a8444da977e74ea330dae87ce243b3ce4d7eb
dldush <leecarus0301@naver.com>
2024-10-02 19:15:01 +0900

    [Silver III] Title: 2×n 타일링 2, Time: 92 ms, Memory: 13032 KB -BaekjoonHub

---
+++ 백준/Silver/11727. 2×n 타일링 2/2×n 타일링 2.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        long[] dp = new long[n + 1];
+
+        if (n == 1) {
+            System.out.println(1);
+            return;  // 종료
+        }
+        if (n == 2) {
+            System.out.println(3);
+            return;  // 종료
+        }
+
+        dp[1] = 1;
+        dp[2] = 3;
+
+        for (int i = 3; i <= n; i++) {
+            dp[i] = (dp[i-1] + (dp[i-2]*2)) % 10007;
+        }
+
+        System.out.println(dp[n]);
+    }
+
+}
