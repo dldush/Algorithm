d33253231eb4d8cf41696726d998c6c5aa6c1a08
dldush <leecarus0301@naver.com>
2025-07-29 13:07:17 +0900

    [level 2] Title: 타겟 넘버, Time: 5.60 ms, Memory: 76.3 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
@@ -1,20 +1,14 @@
 class Solution {
-
-    int[] numbers;
-    int target;
-
     public int solution(int[] numbers, int target) {
-        this.numbers = numbers;
-        this.target = target;
-        return dfs(0, 0);
+        int answer = dfs(0, target, 0, numbers);
+        return answer;
     }
-
-    public int dfs(int index, int sum) {
-        if (index == numbers.length) {
+    public int dfs(int index, int target, int sum, int[] numbers) {
+        
+        if(index == numbers.length){
             return (sum == target) ? 1 : 0;
         }
-
-        return dfs(index + 1, sum + numbers[index]) + dfs(index + 1, sum - numbers[index]);
+        
+        return dfs(index + 1, target, sum + numbers[index], numbers) + dfs(index + 1, target, sum - numbers[index], numbers);
     }
-
 }
