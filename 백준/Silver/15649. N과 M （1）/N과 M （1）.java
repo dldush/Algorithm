36d4b1809ce899595259d7161f848313bec6b4cd
dldush <leecarus0301@naver.com>
2025-09-10 13:25:48 +0900

    [Silver III] Title: N과 M (1), Time: 1616 ms, Memory: 63840 KB -BaekjoonHub

--- 백준/Silver/15649. N과 M （1）/N과 M （1）.java
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.java
@@ -1,42 +1,45 @@
-import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
-	static int N,M;
-	static int []arr;
-	static boolean [] visited;
 
-	public static void main(String[] args) {
-		Scanner sc =new Scanner(System.in);
-		
-		N=sc.nextInt();
-		M=sc.nextInt();
-		
-		arr=new int[M];
-		visited = new boolean[N+1];
-	
-		
-		perm(0);
+    static int N, M;
+    static boolean[] visited;
+    static int[] answer;
 
-	}
+    public static void main(String[] args) {
 
-	static void perm(int cnt) {
-		if(cnt==M) {
-			for(int i=0; i<M; i++) {
-				System.out.print(arr[i]+" ");
-			}System.out.println();
-			return;
-			
-		}
-		
-		for(int i=1; i<=N; i++) {
-			if(!visited[i]) {
-				visited[i]=true;
-				arr[cnt]=i;
-				perm(cnt+1);
-				visited[i]=false;
-			}
-		}
-	}
+        Scanner sc = new Scanner(System.in);
 
+        N = sc.nextInt();
+        M = sc.nextInt();
+        visited = new boolean[N+1];
+        answer = new int[M];
+
+        perm(0);
+
+    }
+
+    public static void perm(int depth) {
+
+        if(depth == M) {
+            for (int i = 0; i < M; i++) {
+                System.out.print(answer[i] + " ");
+            }
+            System.out.println();
+            return;
+        }
+
+        for (int i = 1; i <= N; i++) {
+
+            if (!visited[i]) {
+
+               visited[i] = true;
+               answer[depth] = i;
+               perm(depth + 1);
+               visited[i] = false;
+
+            }
+
+        }
+    }
 }
